fda7bd428d84276793f9ca718ba6d0a3ddd8c26d
Houssnou Kabore <hkabore@wsits.com>
2021-05-09 21:41:03 -0400

    added task components

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,18 @@
+import { Task } from './../../Task';
+import { Component, OnInit } from '@angular/core';
+import { TASKS } from '../../mock-taks';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+ tasks: Task[] = TASKS;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
